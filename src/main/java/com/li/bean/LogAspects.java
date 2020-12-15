37e03765f66663748dc06e350cfbc82a314250f2
lichen <lichne_802@163.com>
2020-12-15 14:33:31 +0900

    Spring-annotation

--- src/main/java/com/li/bean/LogAspects.java
+++ src/main/java/com/li/bean/LogAspects.java
@@ -1,7 +1,10 @@
 package com.li.bean;
 
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+
+import java.util.Arrays;
 
 //切面类
 @Aspect  //告诉Spring容器当前类时切面类
@@ -10,26 +13,29 @@ public class LogAspects {
     //本类引用
     //引用其他类时 需要在执行切入点时写入全类名
     @Pointcut("execution(public int com.li.bean.MathCalculator.*(..))")
-    public void pointCut(){}
+    public void pointCut() {
+    }
 
     //指定切入点
     @Before("pointCut()")
-    public void logStart(){
-        System.out.println("除法运行。。。。。参数列表{}");
+    public void logStart(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        System.out.println(joinPoint.getSignature().getName() + "运行。。。。。参数列表{" + Arrays.asList(args) + "}");
     }
 
     @After("pointCut()")
-    public void logEnd(){
-        System.out.println("除法结束。。。。。参数列表{}");
+    public void logEnd(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        System.out.println("除法结束。。。。。参数列表{" + Arrays.asList(args) + "}");
     }
 
-    @AfterReturning("pointCut()")
-    public void logReturn(){
-        System.out.println("除法返回。。。。。参数列表{}");
+    @AfterReturning(value = "pointCut()", returning = "result")
+    public void logReturn(Object result) {
+        System.out.println("除法返回。。。。。参数列表{" + result + "}");
     }
 
-    @AfterThrowing("pointCut()")
-    public void logException(){
-        System.out.println("除法异常。。。。。异常信息{}");
+    @AfterThrowing(value = "pointCut()", throwing = "exception")
+    public void logException(Exception exception) {
+        System.out.println("除法异常。。。。。异常信息{" + exception + "}");
     }
 }
